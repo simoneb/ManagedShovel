620a6b1855f709edbac0a42aaaf5088ee6aa0191
Simone Busoli <simone.busoli@gmail.com>
2012-03-24 14:06:15 +0100

    Slight changes to build script

--- make.bat
+++ make.bat
@@ -1,17 +1,3 @@
 @echo off
 cls
-
 @%~dp0tools\Pencil\Pencil.exe %~dp0Build.cs %*
-
-if not %ERRORLEVEL% ==0 goto error
-
-rem Success, paint it green.
-	color 2F
-	goto done
-
-:error
-rem Fail!, paint it red.
-	color 4F
-	
-:done
-	color
