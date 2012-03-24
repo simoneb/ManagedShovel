8b9d1c7469fbfcc0cdbcecc57743aa7563814131
Simone Busoli <simone.busoli@gmail.com>
2012-03-24 14:52:06 +0100

    Documented demo

--- src/ManagedShovel.Demo/Program.cs
+++ src/ManagedShovel.Demo/Program.cs
@@ -6,40 +6,56 @@ namespace ManagedShovel.Demo
     {
         static void Main(string[] args)
         {
-            var shovel1 = ManagedShovel.From("amqp://localhost", "amqp://localhost:5677", "amqp://localhost:5674")
-                                     .Declarations(m => m.ExchangeDeclare("TestExchange", "topic"),
-                                                   m => m.QueueBind(m.QueueDeclare(), "TestExchange", "#"))
-                                     .To("amqp://localhost/secondary", "amqp://localhost:5675/secondary", "amqp://localhost:5676/secondary")
-                                     .Declarations(m => m.ExchangeDeclare("TestExchange", "topic"))
-                                     //.UseQueue("someQueue")
-                                     .UseLastCreatedQueue()
-                                     .PrefetchCount(50)
-                                     .AckMode(AckMode.OnConfirm)
-                                     .PublishProperties(p => p.ContentType = "x-protobuf")
-                                     //.PublishFields("myExchange", "myRoutingKey")
-                                     .ReconnectDelay(TimeSpan.FromSeconds(5))
-                                     .MaxHops(0)
-                                     .Start();
+            var shovel = ManagedShovel
+                //                          //
+                // ------ Mandatory ------- //
+                //                          //
 
-            //var shovel2 = ManagedShovel.From("amqp://localhost/secondary")
-            //                         .Declarations(m => m.ExchangeDeclare("TestExchange", "topic"),
-            //                                       m => m.QueueBind(m.QueueDeclare(), "TestExchange", "#"))
-            //                         .To("amqp://localhost/")
-            //                         .Declarations(m => m.ExchangeDeclare("TestExchange", "topic"))
-            //    //.UseQueue("someQueue")
-            //                         .UseLastCreatedQueue()
-            //                         .PrefetchCount(50)
-            //                         .AckMode(AckMode.OnConfirm)
-            //                         .PublishProperties(p => p.ContentType = "x-protobuf")
-            //    //.PublishFields("myExchange", "myRoutingKey")
-            //                         .ReconnectDelay(TimeSpan.FromSeconds(5))
-            //                         .MaxHops(0)
-            //                         .Start();
+                // Sources
+                .From("amqp://localhost", "amqp://localhost:5677", "amqp://localhost:5674")
+
+                // Declarations on source
+                .Declarations(m => m.ExchangeDeclare("TestExchange", "topic"),
+                              m => m.QueueBind(m.QueueDeclare(), "TestExchange", "#"))
+
+                // Destinations
+                .To("amqp://localhost/secondary", "amqp://localhost:5675/secondary", "amqp://localhost:5676/secondary")
+
+                // Declarations on destination
+                .Declarations(m => m.ExchangeDeclare("TestExchange", "topic"))
+
+                // Shovel queue, either specify one (it needs to exist already) or pick the last created
+                //.UseQueue("someQueue")
+                .UseLastCreatedQueue()
+
+                //                          //
+                // ------- Optional ------- //
+                //                          //
+
+                // Prefetch count, 0 (default) means no limit
+                .PrefetchCount(50)
+
+                // Ack mode, defaults to OnConfirm
+                .AckMode(AckMode.OnConfirm)
+
+                // Publish properties, to change or add to the message properties
+                .PublishProperties(p => p.ContentType = "some-content-type")
+
+                // Publish fields, to change exchange and/or routing key of published message
+                //.PublishFields("myExchange", "myRoutingKey")
+
+                // Reconnect delay, defaults to 5 seconds
+                .ReconnectDelay(TimeSpan.FromSeconds(5))
+
+                // Max hops allowed for the message before it is discarded. Defaults to 1
+                .MaxHops(1)
+
+                // Start shovel
+                .Start();
 
             Console.ReadLine();
                          
-            shovel1.Stop();
-            //shovel2.Stop();
+            shovel.Stop();
         }
     }
 }
